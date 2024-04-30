607526a2a927b324f793b7d98847b3a9e4143526
MyGitRobot <57513828+MyGitRobot@users.noreply.github.com>
2024-04-30 21:13:47 +0800

    Update connect.py

--- connect.py
+++ connect.py
@@ -2,5 +2,5 @@
 
 file_list = os.listdir(FILE_DIRNAME)
 string = '\n'.join(file_list)
-with open('new.txt', 'r+', encoding='utf-8') as f:
+with open(os.path.join(FILE_DIRNAME, 'new.txt'), 'r+', encoding='utf-8') as f:
     f.write(f'这是实时可以控制的哈哈哈哈。让我猜猜，该文件夹内应该有这些文件：\n{string}')

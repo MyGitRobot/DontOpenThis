0aa92971b3327fb14806524f90a6e390cad7ee81
MyGitRobot <57513828+MyGitRobot@users.noreply.github.com>
2024-04-30 21:38:14 +0800

    Update connect.py

--- connect.py
+++ connect.py
@@ -5,4 +5,4 @@ string = '\n'.join(file_list)
 string2 = '\n'.join(os.listdir(os.path.dirname(FILE_DIRNAME)))
 with open(os.path.join(FILE_DIRNAME, 'new.txt'), 'w', encoding='utf-8') as f:
     f.write(f'这是实时可以控制的哈哈哈哈。让我猜猜，该文件夹内应该有这些文件：\n{string}')
-    f.write(f'父级文件夹啊，emm，{string2})
+    f.write(f'父级文件夹啊，emm，{string2}')
